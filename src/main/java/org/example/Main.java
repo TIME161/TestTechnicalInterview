f9b91e12728b479c9bfad079f65722a222cc93fb
TIME <opout777@yandex.ru>
2023-06-24 16:17:39 +0300

    work done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
 
+import org.example.model.User;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user1 = new User(17, "Name 1");
+        User user2 = new User(75, "Name 2");
+        User user3 = new User(12, "Name 3");
+        User user5 = new User(12, "Name 3");
+        User user4 = new User(25, "Name 4");
+        Collection<User> user = new ArrayList<>(List.of(user1, user2, user3, user4, user5));
+
+        System.out.println(getUniqeOldestUser(user).toString());
+
     }
 
+    public static User getUniqeOldestUser(Collection<User> userToSort) {
+        Set<User> uniqueUsers = new HashSet<>(userToSort);
+        List<User> userList = new ArrayList<>(uniqueUsers);
+        userList.sort(Comparator.comparing(User::getAge).reversed().thenComparing(User::getName));
+        User oldesUser = userList.get(0);
+
+
+        return oldesUser;
+    }
 
 }
---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,42 @@
+package org.example.model;
+
+import java.util.Objects;
+
+public class User {
+    private int age;
+    private String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
